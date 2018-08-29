709649d6720ac573799865267e7134a64a4eee4c
wangjiawang <15829348578@163.com>
2018-08-29 19:22:26 +0800

    未经重构前的代码

---
+++ mylibrary/src/main/java/com/example/mylibrary/Movie.java
@@ -0,0 +1,36 @@
+package com.example.mylibrary;
+
+/**
+ * Description:影片类
+ * Created by wangjiawang on 2018/8/28.
+ * complete
+ */
+public class Movie {
+    public static final int CHILDRENS = 2;  //儿童片
+    public static final int REGULAR = 0;     //定期
+    public static final int NEW_RELEASE = 1;    //新片
+
+    private String mTitle;      //片名
+    private int mPriceCode;  //价格
+
+    public Movie(String mTitle, int mPriceCode) {
+        this.mTitle = mTitle;
+        this.mPriceCode = mPriceCode;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public int getmPriceCode() {
+        return mPriceCode;
+    }
+
+    public void setmPriceCode(int mPriceCode) {
+        this.mPriceCode = mPriceCode;
+    }
+}
